cdab46cccf76d921e12598933783f641fffabef4
elizabethkim828 <elizabethkim828@gmail.com>
2016-04-26 16:26:35 -0400

    added files

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+
+app.use('/', function (request, response, next) {
+  console.log(request.method + 1234);
+  console.log(request.url);
+  next();
+});
+
+app.use(morgan('combined'));
+
+app.get('/', function (request, response, next) {
+  response.send('Hello World!');
+});
+
+app.get('/news', function (request, response, next) {
+  response.send('Hello News!');
+});
+
+app.post('/modernism', function (request, response, next) {
+  response.send('Hello News!');
+});
+
+app.listen(3000, function () {
+  console.log('server listening...');
+});
